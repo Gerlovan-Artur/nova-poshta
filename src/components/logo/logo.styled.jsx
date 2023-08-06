4cee8f315ac9aa4387513e7b2ebc6ac605fec40c
Gerlovan-Artur <gerlovanartur@gmail.com>
2023-08-06 21:28:16 +0300

    logo tablet add

--- src/components/logo/logo.styled.jsx
+++ src/components/logo/logo.styled.jsx
@@ -1,9 +1,17 @@
 import styled from '@emotion/styled';
 
-export const LogoEl = styled.img`
+export const LogoElM = styled.img`
  width: 100%;
 
  @media screen and (min-width: 768px) {
+ display: none;
+}  
+`;
+
+export const LogoElL = styled.img`
+ width: 100%;
+
+ @media screen and (max-width: 767px) {
  display: none;
 }  
 `;
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,11 +1,15 @@
 import { StyledNavLink, NavEl } from './Navigation.styled';
-import logo from '../../images/Nova_Poshta_2014_logo.svg.png';
-import { LogoEl } from '../../components/logo/logo.styled';
+import logoM from '../../images/Nova_Poshta_2014_logo.svg.png';
+import { LogoElM } from '../../components/logo/logo.styled';
+import logoL from '../../images/logo-nova-poshta.jpg';
+import { LogoElL } from '../../components/logo/logo.styled';
+
 
 export const Nav = () => {
   return (
     <header>
-      <LogoEl src={logo} alt="Logo" />
+      <LogoElM src={logoM} alt="Logo" />
+      <LogoElL src={logoL} alt="Logo" />
       <NavEl>
         <StyledNavLink to="/">Перевірити ТТН</StyledNavLink>
         <StyledNavLink to="/list">Список відділень</StyledNavLink>
