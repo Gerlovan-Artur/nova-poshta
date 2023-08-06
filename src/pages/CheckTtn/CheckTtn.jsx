f43805785ef215ec4dffad20b103d1329f8e159d
Gerlovan-Artur <gerlovanartur@gmail.com>
2023-08-06 16:16:49 +0300

    loader add

--- src/pages/CheckTtn/CheckTtn.jsx
+++ src/pages/CheckTtn/CheckTtn.jsx
@@ -2,13 +2,25 @@ import { InfoCard } from '../../components/card/Card';
 import { RequestHistory } from '../../components/history/History';
 import { SearchForm } from '../../components/form/Form';
 
+import { InfoWrapper } from './CheckTtn.styled';
+import { Loader } from '../../components/loader/Loader';
+
+import { selectIsLoading } from '../../redux/selectors';
+import { useSelector } from 'react-redux';
 
 export default function CheckTtn() {
+  const isLoading = useSelector(selectIsLoading);
   return (
     <>
       <SearchForm />
-      <InfoCard />
-      <RequestHistory />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <InfoWrapper>
+          <InfoCard />
+          <RequestHistory />
+        </InfoWrapper>
+      )}
     </>
   );
 }
